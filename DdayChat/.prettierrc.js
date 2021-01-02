a6c96ac84d58e6669b843c7898a655218d8b75fc
Arakene <kbh97102@naver.com>
2021-01-02 15:21:13 +0900

    Initialize React Project

---
+++ DdayChat/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  rules: {
+    'prettier/prettier': [
+        'error',
+        {
+            endOfLine: 'auto',
+        },
+    ],
+},
+};
